91818466abed44f4448052d739a62ed413409a1f
games <Manish_Admin>
2023-01-11 11:01:44 +0530

    Initial commit

---
+++ app/src/main/java/com/firex/ff_fire_wallpapers/Utilities/Configuration.java
@@ -0,0 +1,8 @@
+package com.firex.ff_fire_wallpapers.Utilities;
+
+import android.os.Environment;
+
+public class Configuration {
+    public static String appFolderPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pictures/FF Wallpapers/";
+    public static String appFolderName = "FF Wallpapers";
+}
